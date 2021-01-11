b8f4235f23e1743354ce682ed762304211ceb89a
amit-k-umar <61649790+amit-k-umar@users.noreply.github.com>
2021-01-11 17:19:18 +0530

    Updates

---
+++ precache-manifest.b16e18a06bca60e635324ed36bdbeadc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1575834d744a8d7c9074a9bb676484",
+    "url": "/spacexconsole/index.html"
+  },
+  {
+    "revision": "e94e043f506768e2a702",
+    "url": "/spacexconsole/static/css/main.fbd972af.chunk.css"
+  },
+  {
+    "revision": "b6f69afb99f56a36c04d",
+    "url": "/spacexconsole/static/js/2.faa71e96.chunk.js"
+  },
+  {
+    "revision": "3c56d32f1a0e992453f57615992b86b8",
+    "url": "/spacexconsole/static/js/2.faa71e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e94e043f506768e2a702",
+    "url": "/spacexconsole/static/js/main.75e6ea68.chunk.js"
+  },
+  {
+    "revision": "8d5b577927147870d8c4",
+    "url": "/spacexconsole/static/js/runtime-main.f7e510cb.js"
+  },
+  {
+    "revision": "dd8520ed969ba3587333e0169462adbd",
+    "url": "/spacexconsole/static/media/logo.dd8520ed.svg"
+  },
+  {
+    "revision": "e45347368c546e62abc5ac784183ddd9",
+    "url": "/spacexconsole/static/media/logo.e4534736.png"
+  },
+  {
+    "revision": "d04e25268283f4ecc13544f3ebb952e8",
+    "url": "/spacexconsole/static/media/logo_x.d04e2526.svg"
+  },
+  {
+    "revision": "4f0ee44849485cb1426d53f55b04e087",
+    "url": "/spacexconsole/static/media/spacexconsole_logo.4f0ee448.svg"
+  }
+]);
